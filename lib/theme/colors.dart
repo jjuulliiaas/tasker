f50ad10b98971ccc04f9b0327148bd5b84564fb2
jjuulliiaas <sidlovska.julia2018@gmail.com>
2024-10-30 05:40:40 -0700

    - add new directory 'theme'; - add file 'colors.dart' with list of colors for future using at this directory.

---
+++ lib/theme/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ColorsList {
+
+  // dark colors:
+  static const Color kDarkGreen = Color(0x385F6A);
+
+  // light colors:
+  static const Color kLightGreen = Color(0x6A8DA2);
+  static const Color kLightestGreen = Color(0x90A8C3);
+  static const Color kAppBackground = Color(0xF9F9F9);
+  static const Color kAuthBackground = Color(0xF9F9F9);
+  static const Color kGrey = Color(0xD9D9D9);
+
+}
