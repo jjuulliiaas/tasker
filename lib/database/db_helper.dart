671430078c961d0ab54b28748acaa1cbe2d86578
jjuulliiaas <sidlovska.julia2018@gmail.com>
2024-11-12 11:05:05 -0800

    - add method for sign up user at class db_helper.dart

--- lib/database/db_helper.dart
+++ lib/database/db_helper.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:flutter/services.dart';
+import 'package:crypto/crypto.dart';
+import 'dart:convert';
 
 class DatabaseHelper {
   static final DatabaseHelper instance = DatabaseHelper._init();
@@ -38,21 +40,46 @@ class DatabaseHelper {
     return await openDatabase(path);
   }
 
-  // create task
   Future<int> createTask(Map<String, dynamic> task) async {
     final db = await instance.database;
     return await db.insert('tasks', task);
   }
 
-  // load tasks from db
   Future<List<Map<String, dynamic>>> readAllTasks() async {
     final db = await instance.database;
     return await db.query('tasks');
   }
 
-  // close db
   Future close() async {
     final db = await instance.database;
     db.close();
   }
+
+
+
+
+  // sign up
+  Future<int> userSignUp(String name, String email, String password) async {
+    final db = await instance.database;
+
+    final existingUser = await db.query(
+      'users',
+      where: 'user_email = ?',
+      whereArgs: [email],
+    );
+    if (existingUser.isNotEmpty) {
+      throw Exception("This e-mail has already registered.");
+    }
+
+    // hash password
+    final hashedPassword = sha256.convert(utf8.encode(password)).toString();
+
+    final user = {
+      'user_name': name,
+      'user_email': email,
+      'user_password': hashedPassword,
+    };
+    return await db.insert('users', user);
+  }
+
 }
